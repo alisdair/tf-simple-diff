8f1e69c424a866a78221e363775ca15f32528d19
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2021-07-14 14:28:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,20 @@ variable "foo" {
   default = "{\"boop\":\"beep\"}"
 }
 
+variable "bar" {
+  type = string
+  default = "honk"
+}
+
+variable "baz" {
+  type = string
+  default = "[1,2,3,4]"
+}
+
 resource "null_resource" "none" {
   triggers = {
     foo = var.foo
+    bar = var.bar
+    baz = var.baz
   }
 }
